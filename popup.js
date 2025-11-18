b1ecbcbe853c5cbe7c4750736e6d0345862fd422
trainerpb <soham.payback@gmail.com>
2025-11-18 22:35:01 +0530

    A working chat extension with makeoveron UI.Disabled chunking of page content in the current tab for now

--- popup.js
+++ popup.js
@@ -1,3 +1,50 @@
+const chat = document.getElementById('chat');
+const input = document.getElementById('input');
+
+const API="http://localhost:8080/api/chat/whatsapp-stream?q="
+input.addEventListener('keydown', (e) => {
+    console.log("Keydown ---------------------------------");
+  if (e.key === 'Enter' && input.value.trim().length>0) {
+    const userMsg = input.value.trim();
+    appendMessage(userMsg, 'user');
+    input.value = '';
+
+    // // Simulate bot response
+    // setTimeout(() => {
+    //   appendMessage("You said: " + userMsg, 'bot');
+    // }, 500);
+   var source = new EventSource(API+encodeURIComponent(userMsg));
+   const answerDiv= appendMessage('Reply: ','bot');
+   source.onmessage = function(event) {
+        let chunk=event.data;
+        answerDiv.textContent += ' '+ chunk;
+   };
+
+   source.onerror = () => {
+    console.log("\n\n❌ Stream disconnected.");
+    source.close();
+   
+  };
+source.addEventListener('done', (event) => {
+  console.log('Stream ended:', event.data);
+  source.close();
+ 
+});
+
+  }
+});
+
+function appendMessage(text, sender) {
+  const msg = document.createElement('div');
+  msg.className = `msg ${sender}`;
+  msg.textContent = text;
+  chat.appendChild(msg);
+  chat.scrollTop = chat.scrollHeight;
+  return msg;
+}
+
+
+
 document.getElementById('clickMe').addEventListener('click', () => {
     alert('Button clicked!');
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
