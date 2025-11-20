c01dfed575b5c8c2cc62159d6524e0cc0c6a93a4
SohamSirOnline <sohamsironline@gmail.com>
2025-11-20 14:06:08 +0530

    Added side panel

--- popup.js
+++ popup.js
@@ -1,46 +1,54 @@
 const chat = document.getElementById('chat');
 const input = document.getElementById('input');
 
-const API="http://localhost:8080/api/chat/whatsapp-stream?q="
+const API = "http://192.168.137.84:8080/api/chat/whatsapp-stream?q="
 input.addEventListener('keydown', (e) => {
     console.log("Keydown ---------------------------------");
-  if (e.key === 'Enter' && input.value.trim().length>0) {
-    const userMsg = input.value.trim();
-    appendMessage(userMsg, 'user');
-    input.value = '';
+    if (e.key === 'Enter' && input.value.trim().length > 0) {
+        const userMsg = input.value.trim();
+        appendMessage(userMsg, 'user');
+        input.value = '';
 
-    // // Simulate bot response
-    // setTimeout(() => {
-    //   appendMessage("You said: " + userMsg, 'bot');
-    // }, 500);
-   var source = new EventSource(API+encodeURIComponent(userMsg));
-   const answerDiv= appendMessage('Reply: ','bot');
-   source.onmessage = function(event) {
-        let chunk=event.data;
-        answerDiv.textContent += ' '+ chunk;
-   };
+        // // Simulate bot response
+        // setTimeout(() => {
+        //   appendMessage("You said: " + userMsg, 'bot');
+        // }, 500);
+        var source = new EventSource(API + encodeURIComponent(userMsg));
+        const answerDiv = appendMessage('Reply: ', 'bot');
+        source.onmessage = function (event) {
+            let chunk = event.data;
+            answerDiv.textContent += ' ' + chunk;
+        };
 
-   source.onerror = () => {
-    console.log("\n\n❌ Stream disconnected.");
-    source.close();
-   
-  };
-source.addEventListener('done', (event) => {
-  console.log('Stream ended:', event.data);
-  source.close();
- 
+        source.onerror = () => {
+            console.log("\n\n❌ Stream disconnected.");
+            source.close();
+
+        };
+        source.addEventListener('done', (event) => {
+            console.log('Stream ended:', event.data);
+            source.close();
+
+        });
+
+    }
 });
 
-  }
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.sidePanel.setOptions({
+    path: "popup.html",
+    enabled: true
+  });
 });
+
 
 function appendMessage(text, sender) {
-  const msg = document.createElement('div');
-  msg.className = `msg ${sender}`;
-  msg.textContent = text;
-  chat.appendChild(msg);
-  chat.scrollTop = chat.scrollHeight;
-  return msg;
+    const msg = document.createElement('div');
+    msg.className = `msg ${sender}`;
+    msg.textContent = text;
+    chat.appendChild(msg);
+    chat.scrollTop = chat.scrollHeight;
+    return msg;
 }
 
 
